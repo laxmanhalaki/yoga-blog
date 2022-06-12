c6071202971609f571435fdcd41891f4affe75e0
laxmanhalaki <107349832+laxmanhalaki@users.noreply.github.com>
2022-06-12 16:44:36 +0530

    blog
    blogs on yoga

---
+++ index.js
@@ -0,0 +1,7 @@
+let navbar=document.querySelector("#navbar");
+document.querySelector("#nav-close").onclick=()=>{
+    navbar.classList.toggle("active");
+}
+document.querySelector("#menu").onclick=()=>{
+    navbar.classList.toggle("active");
+}
